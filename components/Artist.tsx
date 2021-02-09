7d6a36844c261a33baf4f9bbf0e2c41a305d29aa
dreamnyt <aacevski@gmail.com>
2021-02-10 00:01:09 +0100

    Initial Commit

--- components/Artist.tsx
+++ components/Artist.tsx
@@ -2,14 +2,8 @@ import React from "react";
 
 const Artist = ({ artist }) => {
   return (
-    <div
-      className="mt-8 w-movieCard bg-white text-black dark dark:text-white transition duration-500 dark:bg-gray-primary m-4 relative overflow-hidden rounded-sm shadow-lg"
-      id="movieCard"
-    >
-      <img
-        src="https://kgo.googleusercontent.com/profile_vrt_raw_bytes_1587515396_10843.jpg"
-        className="w-full"
-      ></img>
+    <div className="mt-8 w-movieCard bg-white text-black dark dark:text-white transition duration-500 dark:bg-gray-primary m-4 relative overflow-hidden rounded-sm shadow-lg">
+      <img src={artist.images[0]} className="w-full"></img>
       <div className="text-textInfo flex items-center justify-between pt-4 pl-4 pb-4 pr-4 tracking-wider">
         <h4 className="text-lg font-semibold m-0">{artist.name}</h4>
         <span
@@ -17,7 +11,7 @@ const Artist = ({ artist }) => {
             "bg-span-light rounded-sm font-bold pt-1 pl-2 pb-1 pr-2 text-lg text-white dark:text-white"
           }
         >
-          1
+          {artist.index}
         </span>
       </div>
     </div>
